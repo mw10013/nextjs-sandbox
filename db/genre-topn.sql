a8c81df11363499b46c2023523a1e832c2f8ce85
Michael Wu <mw10013@gmail.com>
2022-10-13 16:47:42 -0400

    formatting

--- db/genre-topn.sql
+++ db/genre-topn.sql
@@ -1,8 +1,7 @@
 /* @name FindGenreTopN */
 -- name: genre-top-n
 -- Get the N top tracks by genre
-select
-    genre.name as genre,
+select genre.name as genre,
     case when length(ss.name) > 15 then
         substring(ss.name from 1 for 15) || '...'
     else
@@ -10,28 +9,21 @@ select
     end as track,
     ss.count as count,
     artist.name as artist
-from
-    genre
+from genre
     left join lateral (
-        select
-            track.name,
+        select track.name,
             track.albumid,
             count(playlistid)
-        from
-            track
+        from track
             left join playlisttrack using (trackid)
-        where
-            track.genreid = genre.genreid
-        group by
-            track.trackid
-        order by
-            count desc
+        where track.genreid = genre.genreid
+        group by track.trackid
+        order by count desc
         limit :n) as ss (name,
         albumid,
         count) on true
     join album using (albumid)
     join artist using (artistid)
-order by
-    genre.name,
+order by genre.name,
     ss.count desc;
 
--- db/inspired-artists.sql
+++ db/inspired-artists.sql
@@ -1,24 +1,12 @@
 /* @name FindInspiredArtists */
 -- artists names used as track names by other artists
-select
-    artist.name as artist,
-    -- "inspired" is the other artist
+select artist.name as artist,
     inspired.name as inspired,
     album.title as album,
     track.name as track
-from
-    artist
-    /*
-     * Here we join the artist name on the track name,
-     * which is not our usual kind of join and thus
-     * we don't use the using() syntax. For
-     * consistency and clarity of the query, we use
-     * the "on" join condition syntax through the
-     * whole query.
-     */
+from artist
     join track on track.name = artist.name
     join album on album.albumid = track.albumid
     join artist inspired on inspired.artistid = album.artistid
-where
-    artist.artistid <> inspired.artistid;
+where artist.artistid <> inspired.artistid;
 
