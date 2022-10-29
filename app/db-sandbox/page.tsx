14f221ba66810ffa990159e4b20d2bcc582b825a
Michael Wu <mw10013@gmail.com>
2022-10-29 16:54:47 -0400

    findMany

--- app/db-sandbox/page.tsx
+++ app/db-sandbox/page.tsx
@@ -2,6 +2,7 @@ import { pgTypedClient } from "../../db";
 import { getAccessPoint } from "../../db/get_access_point.queries";
 import { IDatabaseConnection, PreparedQuery } from "@pgtyped/query/lib/tag";
 import { getAccessHub } from "../../db/get_access_hub.queries";
+import { getAccessUsersByPoint } from "../../db/get_access_users_by_point.queries";
 
 async function findUniqueOrThrow<TParamType, TResultType>(
   preparedQuery: PreparedQuery<TParamType, TResultType>,
@@ -18,6 +19,14 @@ async function findUniqueOrThrow<TParamType, TResultType>(
   }
 }
 
+async function findMany<TParamType, TResultType>(
+  preparedQuery: PreparedQuery<TParamType, TResultType>,
+  params: TParamType,
+  conn: IDatabaseConnection
+) {
+  return preparedQuery.run(params, conn);
+}
+
 async function fetchData() {
   const accessPoint = await findUniqueOrThrow(
     getAccessPoint,
@@ -28,19 +37,28 @@ async function fetchData() {
     },
     pgTypedClient
   );
-  const hub = await findUniqueOrThrow(getAccessHub, {accessHubId: 4, authUserId: "733e54ae-c9dc-4b9a-94d0-764fbd1bd76e"},
-  pgTypedClient);
+  const accessHub = await findUniqueOrThrow(
+    getAccessHub,
+    { accessHubId: 4, authUserId: "733e54ae-c9dc-4b9a-94d0-764fbd1bd76e" },
+    pgTypedClient
+  );
+  const accessUsers = await findMany(
+    getAccessUsersByPoint,
+    { accessPointId: 14 },
+    pgTypedClient
+  );
 
-  return { dt: new Date().toISOString(), accessPoint, hub };
+  return { dt: new Date().toISOString(), accessPoint, accessHub, accessUsers };
 }
 
 export default async function Page() {
-  const { dt, accessPoint, hub } = await fetchData();
+  const { dt, accessPoint, accessHub, accessUsers } = await fetchData();
   return (
     <div>
       <p>{dt}</p>
       <pre>{JSON.stringify(accessPoint, null, 2)}</pre>
-      <pre>{JSON.stringify(hub, null, 2)}</pre>
+      <pre>{JSON.stringify(accessHub, null, 2)}</pre>
+      <pre>{JSON.stringify(accessUsers, null, 2)}</pre>
     </div>
   );
 }
