2e50f67e15160816d468bf14ea1a3ed55012ccf1
Michael Wu <mw10013@gmail.com>
2022-10-30 22:03:56 -0400

    wip

--- app/db-sandbox/page.tsx
+++ app/db-sandbox/page.tsx
@@ -1,55 +1,19 @@
-import { pgTypedClient } from "../../db";
+import { findMany, findUniqueOrThrow, pgTypedClient } from "../../db";
 import { getAccessPoint } from "../../db/get_access_point.queries";
-import { IDatabaseConnection, PreparedQuery } from "@pgtyped/query/lib/tag";
 import { getAccessHub } from "../../db/get_access_hub.queries";
 import { getAccessUsersByPoint } from "../../db/get_access_users_by_point.queries";
 import _ from "lodash";
 
-/* 
-pgTyped camelCaseColumnNames config seems to be implemented on types, but not values.
-PreparedQuery.run returns results that may not align with its type. 
-We mutate results as needed for camelCaseColumnNames.
-Note that pgTyped mapQueryResultRows also mutates.
-*/
-
-function camelCaseResultMutation(result: Record<string, unknown>) {
-  for (const k of Object.keys(result)) {
-    if (k.indexOf("_")) {
-      result[_.camelCase(k)] = result[k];
-      delete result[k];
-    }
-  }
-}
-
-async function findUniqueOrThrow<TParamType, TResultType>(
-  preparedQuery: PreparedQuery<TParamType, TResultType>,
-  params: TParamType,
-  conn: IDatabaseConnection
-) {
-  const results = await preparedQuery.run(params, conn);
-  if (results.length === 1) {
-    camelCaseResultMutation(results[0] as Record<string, unknown>);
-    return results[0];
-  } else if (results.length === 0) {
-    throw new Error(`findUnique: empty results`);
-  } else {
-    throw new Error(`findUnique: >1 row in results (${results.length} rows)`);
-  }
-}
-
-async function findMany<TParamType, TResultType>(
-  preparedQuery: PreparedQuery<TParamType, TResultType>,
-  params: TParamType,
-  conn: IDatabaseConnection
-) {
-  const results = await preparedQuery.run(params, conn);
-  for (const result of results) {
-    camelCaseResultMutation(result as Record<string, unknown>);
-  }
-  return results;
-}
-
-async function fetchData() {
+async function fetchData(): Promise<{
+  accessPoint: {
+    accessHub: import("/home/mw10013/src/nextjs-sandbox/db/get_access_hub.queries").IGetAccessHubResult;
+    accessUsers: import("/home/mw10013/src/nextjs-sandbox/db/get_access_users_by_point.queries").IGetAccessUsersByPointResult[];
+    accessHubId: number;
+    accessPointId: number;
+    name: string;
+    position: number;
+  };
+}> {
   const accessPoint = await findUniqueOrThrow(
     getAccessPoint,
     {
@@ -71,16 +35,14 @@ async function fetchData() {
   );
 
   return {
-    dt: new Date().toISOString(),
     accessPoint: { ...accessPoint, accessHub, accessUsers },
   };
 }
 
 export default async function Page() {
-  const { dt, accessPoint } = await fetchData();
+  const { accessPoint } = await fetchData();
   return (
     <div>
-      <p>{dt}</p>
       <pre>{JSON.stringify(accessPoint, null, 2)}</pre>
     </div>
   );
--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,6 @@
 import "server-only";
 import { Pool } from "pg";
-import { IDatabaseConnection } from "@pgtyped/query/lib/tag";
+import { IDatabaseConnection, PreparedQuery } from "@pgtyped/query/lib/tag";
 
 // https://www.prisma.io/docs/guides/database/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
 
@@ -32,10 +32,53 @@ export const pgTypedClient: IDatabaseConnection = {
   query(query: string, bindings: any[]) {
     if (process.env.NODE_ENV === "production") {
       console.log({ query });
-    }
-    else {
-      console.log({query, bindings})
+    } else {
+      console.log({ query, bindings });
     }
     return pgPool.query(query, bindings);
   },
 };
+
+/* 
+pgTyped camelCaseColumnNames config seems to be implemented on types, but not values.
+PreparedQuery.run returns results that may not align with its type. 
+We mutate results as needed for camelCaseColumnNames.
+Note that pgTyped mapQueryResultRows also mutates.
+*/
+
+function camelCaseResultMutation(result: Record<string, unknown>) {
+  for (const k of Object.keys(result)) {
+    if (k.indexOf("_")) {
+      result[_.camelCase(k)] = result[k];
+      delete result[k];
+    }
+  }
+}
+
+export async function findUniqueOrThrow<TParamType, TResultType>(
+  preparedQuery: PreparedQuery<TParamType, TResultType>,
+  params: TParamType,
+  conn: IDatabaseConnection
+) {
+  const results = await preparedQuery.run(params, conn);
+  if (results.length === 1) {
+    camelCaseResultMutation(results[0] as Record<string, unknown>);
+    return results[0];
+  } else if (results.length === 0) {
+    throw new Error(`findUnique: empty results`);
+  } else {
+    throw new Error(`findUnique: >1 row in results (${results.length} rows)`);
+  }
+}
+
+export async function findMany<TParamType, TResultType>(
+  preparedQuery: PreparedQuery<TParamType, TResultType>,
+  params: TParamType,
+  conn: IDatabaseConnection
+) {
+  const results = await preparedQuery.run(params, conn);
+  for (const result of results) {
+    camelCaseResultMutation(result as Record<string, unknown>);
+  }
+  return results;
+}
